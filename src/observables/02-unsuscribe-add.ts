874ff39eb6f015778bcca27c5fbcf1f040e1b50d
Iván Arenas Morante <arenasmorante@gmail.com>
2023-03-05 10:45:09 +0100

    con explicaciones

--- src/observables/02-unsuscribe-add.ts
+++ src/observables/02-unsuscribe-add.ts
@@ -2,12 +2,14 @@ import { Observable } from "rxjs";
 import { Subscriber } from "rxjs/internal/Subscriber";
 import { Observer } from "rxjs/internal/types";
 
+// Estructura observable, es como se va a comportar el suscrptor
 const observer: Observer<any> = {
     next: value => console.log('siguiente:', value),
     error: error => console.warn('error: ', error),
     complete: () => console.info('Completado: ')
 }
 
+// Observable
 const intervalo$ = new Observable<number>( subs => {
 
     let contador = 0;
@@ -27,6 +29,9 @@ const intervalo$ = new Observable<number>( subs => {
         console.log('Intervalo destruido')
     }
 })
+//
+// Suscripcion al observable y se le pasa el comportamiento (observer)
+//
 
 // const subscripcion1 = intervalo$.subscribe( num => console.log('Num: ', num));
 // const subscripcion2 = intervalo$.subscribe( num => console.log('Num: ', num));
@@ -35,9 +40,11 @@ const subscripcion1 = intervalo$.subscribe( observer);
 const subscripcion2 = intervalo$.subscribe( observer);
 const subscripcion3 = intervalo$.subscribe( observer);
 
+// agrupamos suscripciones para gestionarlas juntas
 subscripcion1.add(subscripcion2)
 subscripcion1.add(subscripcion3)
 
+// Nos des-suscribimos
 setTimeout(() => {
      subscripcion1.unsubscribe()
     // subscripcion2.unsubscribe()
